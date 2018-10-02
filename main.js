f08c2c143e55c1843ac70a7664d1faa2ab4e5495
The Goose <trevordammon@gmail.com>
2018-10-02 08:36:21 -0500

    "working image button"

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log('js');
+$(document).ready(function(){
+    let $url;
+    $('#gameImage').keypress(function(e){
+        if(e.keyCode == 13){
+            $url = $('#gameImage').val();
+            console.log($url);
+            $('#gameImage').val('');
+            $('#image').css("background-image", `url(${$url})`);
+            for(let i=0;i<20;i++){
+                console.log(i);
+                $('#image').append('<div class="piece" id= "segment'+i+'"></div>')
+            }
+        }
+    });
+    
+
+})

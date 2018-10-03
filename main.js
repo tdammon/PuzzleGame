7320e3b507a20e09082c71ef52de9e22f1171ea1
The Goose <trevordammon@gmail.com>
2018-10-02 19:39:19 -0500

    Added child division

--- main.js
+++ main.js
@@ -4,14 +4,26 @@ $(document).ready(function(){
         if(e.keyCode == 13){
             $url = $('#gameImage').val();
             console.log($url);
-            $('#gameImage').val('');
-            $('#image').css("background-image", `url(${$url})`);
-            for(let i=0;i<20;i++){
-                console.log(i);
+            for(let i=0;i<16;i++){
+                let x = ((i%4) * boxSize)
+                let y = (Math.floor(i/4) * boxSize)
+            
                 $('#image').append('<div class="piece" id= "segment'+i+'"></div>')
+                document.getElementById("segment"+i).style.width = `${boxSize}`;
+                document.getElementById("segment"+i).style.height = `${boxSize}`;
+                $('#segment'+i).css("backgroundImage", `url(${$url})`);
+                document.getElementById("segment"+i).style.backgroundPosition = `${x}px ${y}px`;
             }
         }
     });
     
 
 })
+
+sq = 4,
+
+$puzzle = $("#image"),
+size = $puzzle.width(),
+boxSize = size / sq;
+
+

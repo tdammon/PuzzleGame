36978130bdb1b3427f11492c4efae3c04c4e96ff
The Goose <trevordammon@gmail.com>
2018-10-07 12:50:26 -0500

    Movability

--- main.js
+++ main.js
@@ -1,29 +1,65 @@
 $(document).ready(function(){
-    let $url;
+    let sq = 4
+    let size = $("#image").width()
+    let boxSize = size / sq
+    let $url
+    let pieceArray = []
+    let clicked = false;
+    let firstPiece, secondPiece;
+    let swap;
+
+    console.log(pieceArray)
     $('#gameImage').keypress(function(e){
         if(e.keyCode == 13){
             $url = $('#gameImage').val();
             console.log($url);
             for(let i=0;i<16;i++){
+                // console.log(pieceArray);
                 let x = ((i%4) * boxSize)
                 let y = (Math.floor(i/4) * boxSize)
-            
-                $('#image').append('<div class="piece" id= "segment'+i+'"></div>')
-                document.getElementById("segment"+i).style.width = `${boxSize}`;
-                document.getElementById("segment"+i).style.height = `${boxSize}`;
-                $('#segment'+i).css("backgroundImage", `url(${$url})`);
-                document.getElementById("segment"+i).style.backgroundPosition = `${x}px ${y}px`;
+                //$('#image').css("backgroundImage", `url(${$url})`);
+                pieceArray.push(`<div style="width: ${boxSize}px; height:${boxSize}px; background-image:url(${$url}); background-position:-${x}px -${y}px" class="piece" id="segment${i}"></div>`)
+                
             }
+            $('#image').append(pieceArray);
         }
     });
+
+
+    $(document).on('click', '.piece', function(){
+        if(!clicked){
+            firstPiece = this;
+            clicked = true;
+            console.log(firstPiece);
+        } else {
+            secondPiece = this;
+            clicked = false
+            console.log(secondPiece);
+            for(pieces in pieceArray){//find the index of firstPiece.id
+                if(pieceArray[pieces].indexOf(firstPiece.id)> -1){
+                    console.log(pieces);
+                    for(secondpieces in pieceArray){
+                        if(pieceArray[secondpieces].indexOf(secondPiece.id)> -1){
+                            console.log(secondpieces)
+                            swap = pieceArray[pieces];
+                            pieceArray[pieces] = pieceArray[secondpieces];
+                            pieceArray[secondpieces] = swap;
+                            $('#image').empty();
+                            $('#image').append(pieceArray);
+                            return
+                        }
+                    }
+                } else {
+                }
+            }
+        }
+
+    })
+    
     
 
 })
 
-sq = 4,
 
-$puzzle = $("#image"),
-size = $puzzle.width(),
-boxSize = size / sq;
 
 

62a7c55c2352d08dbda8f7f259386914b2c66af2
The Goose <trevordammon@gmail.com>
2018-10-07 18:07:49 -0500

    Shuffle Pieces

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
     let pieceArray = []
     let clicked = false;
     let firstPiece, secondPiece;
+    let pieceInd1, pieceInd2;
     let swap;
 
     console.log(pieceArray)
@@ -13,53 +14,86 @@ $(document).ready(function(){
         if(e.keyCode == 13){
             $url = $('#gameImage').val();
             console.log($url);
-            for(let i=0;i<16;i++){
+            for(let i=0;i<15;i++){
                 // console.log(pieceArray);
                 let x = ((i%4) * boxSize)
                 let y = (Math.floor(i/4) * boxSize)
                 //$('#image').css("backgroundImage", `url(${$url})`);
-                pieceArray.push(`<div style="width: ${boxSize}px; height:${boxSize}px; background-image:url(${$url}); background-position:-${x}px -${y}px" class="piece" id="segment${i}"></div>`)
+                pieceArray.push(`<div style="width: ${boxSize}px; height:${boxSize}px; background-image:url(${$url}); background-position:-${x}px -${y}px" class="piece" id="segment${i}X"></div>`)
                 
             }
+            pieceArray.push(`<div style="width: ${boxSize}px; height:${boxSize}px; background-color:black" class="piece" id="blank"></div>`);
+
+            shuffleArray(pieceArray);
             $('#image').append(pieceArray);
+            
         }
     });
 
 
     $(document).on('click', '.piece', function(){
         if(!clicked){
-            firstPiece = this;
             clicked = true;
-            console.log(firstPiece);
+            firstPiece = this;
+            console.log('first Piece ID' +firstPiece.id);
         } else {
-            secondPiece = this;
             clicked = false
-            console.log(secondPiece);
-            for(pieces in pieceArray){//find the index of firstPiece.id
-                if(pieceArray[pieces].indexOf(firstPiece.id)> -1){
-                    console.log(pieces);
-                    for(secondpieces in pieceArray){
-                        if(pieceArray[secondpieces].indexOf(secondPiece.id)> -1){
-                            console.log(secondpieces)
-                            swap = pieceArray[pieces];
-                            pieceArray[pieces] = pieceArray[secondpieces];
-                            pieceArray[secondpieces] = swap;
-                            $('#image').empty();
-                            $('#image').append(pieceArray);
-                            return
-                        }
-                    }
-                } else {
+            secondPiece = this;
+            console.log('Second Piece ID' +secondPiece.id);
+            for(let i=0; i< pieceArray.length;i++){//find the index of firstPiece.id
+                if(pieceArray[i].indexOf(firstPiece.id)> -1){
+                    pieceInd1 = i;
+                    console.log('first Piece Index' +pieceInd1);
+                }
+                if(pieceArray[i].indexOf(secondPiece.id)> -1){
+                    pieceInd2 = i;
+                    console.log('second Piece Index' +pieceInd2);
                 }
             }
+            if((firstPiece.id == "blank" || secondPiece.id == "blank")&&
+                (pieceInd1 == (pieceInd2 +4) || pieceInd1 == (pieceInd2 -4) ||
+                (pieceInd1 == (pieceInd2 +1) && Math.floor(pieceInd1/4) == Math.floor(pieceInd2/4)) || 
+                (pieceInd1 == (pieceInd2 -1) && Math.floor(pieceInd1/4) == Math.floor(pieceInd2/4)))) {
+                swap = pieceArray[pieceInd1];
+                pieceArray[pieceInd1] = pieceArray[pieceInd2];
+                pieceArray[pieceInd2] = swap;
+                $('#image').empty();
+                $('#image').append(pieceArray);
+                firstPiece = null;
+                secondPiece= null;
+                clicked = false;
+                checkForWin(pieceArray);
+                return
+            } else {
+                console.log(pieceInd2 +1 , pieceInd2 -1)
+            }
+        
         }
-
     })
-    
-    
-
 })
 
-
-
-
+let x;
+let y;
+function shuffleArray(array) {
+    for(let i=0;i<array.length;i++){
+        x= Math.floor(Math.random()*array.length);
+        y= array[i];
+        array[i]=array[x];
+        array[x]=y;
+    }
+}
+let test = 0;
+function checkForWin(array) {
+    for(let i=0; i<array.length;i++){
+        if(array[i].includes(`segment${i}X`)){
+            test++
+            console.log('Yes')
+        } else {
+            test=0;
+            break;
+        }
+        if(test == 15){
+            console.log('Win');
+        }
+    }
+}
